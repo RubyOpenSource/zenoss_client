4c1717325efbaa4805c81425a1d244136124ab58
Dan Wanek <dan.wanek@gmail.com>
2010-03-08 10:01:33 -0600

    method additions

--- lib/model/device.rb
+++ lib/model/device.rb
@@ -69,24 +69,113 @@ module Zenoss
         rest('getHWProductName')
       end
 
-      def get_system_names
-        plist_to_array( rest('getSystemNames') )
+      # Get the HTML formatted link to the Location that this devices exists at.
+      # If you are looking for just the name, use #get_location_name instead.
+      def get_location_link
+        rest('getLocationLink')
+      end
+
+      # Returns a String value of the Location that this device exists at.  You can
+      # also issue #get_location_link if you want an HTML link to the location page.
+      def get_location_name
+        rest('getLocationName')
+      end
+
+      # Returns data ready for serialization
+      def get_data_for_json
+        rest('getDataForJSON')
       end
 
       def get_device_group_names
         plist_to_array( rest('getDeviceGroupNames') )
       end
 
+      # Returns a DateTime instance when this device was last modified.
       def get_last_change
         pdatetime_to_datetime( rest('getLastChange') )
       end
 
+      # Return the management ip for this device.
       def get_manage_ip
         rest('getManageIp')
       end
 
+      # Returns the name of the Zenoss Collector that this host currently belogs to.
+      def get_performance_server_name
+        rest('getPerformanceServerName')
+      end
+
+      # Return the pingStatus as a string
+      def get_ping_status_string
+        rest('getPingStatusString')
+      end
+
+      def get_pretty_link
+        rest('getPrettyLink')
+      end
+
+      # Return the numeric device priority.
+      def get_priority
+        (rest('getPriority')).to_i
+      end
+
+      # Return the device priority as a string.
+      def get_priority_string
+        rest('getPriorityString')
+      end
+
+      # Return the prodstate as a string.
+      def get_production_state_string
+        rest('getProductionStateString')
+      end
+
+      def get_rrd_templates
+      end
+
+      # Returns a DateTime instance when this device was last collected from.
       def get_snmp_last_collection
         pdatetime_to_datetime( rest('getSnmpLastCollection') )
+      end
+
+      # Return the snmpStatus as a string.
+      def get_snmp_status_string
+        rest('getSnmpStatusString')
+      end
+
+      # Returns an Array of Zenoss /Systems that this device belogs to.
+      def get_system_names
+        plist_to_array( rest('getSystemNames') )
+      end
+
+      # Return the Zenoss /Systems that this device belogs to in a string
+      # separated by "sep".
+      def get_system_names_string(sep=',')
+        rest("getSystemNamesString?sep=#{sep}")
+      end
+
+      # Returns true if the device production state >= zProdStateThreshold.
+      def monitor_device?
+        (rest('monitorDevice')).eql?('True') ? true : false
+      end
+
+      # Returns true if the device is subject to SNMP monitoring
+      def snmp_monitor_device?
+        (rest('snmpMonitorDevice')).eql?('True') ? true : false
+      end
+
+      # Return the SNMP uptime
+      def uptime_str
+      end
+
+      # Update the devices hardware tag with the passed string
+      def set_hw_tag(asset_tag)
+        puts "No REST implementation yet."
+        #rest("setHWTag?assettag#{asset_tag}")
+      end
+
+      # -------- Methods from DeviceResultInt.DeviceResultInt -------- #
+      def get_device_class_name
+        rest('getDeviceClassName')
       end
 
       protected
