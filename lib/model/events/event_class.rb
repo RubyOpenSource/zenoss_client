1915e6dfa6c493cbb839392832f973f0f4481c0a
Dan Wanek <dan.wanek@gmail.com>
2010-03-23 12:16:36 -0500

    Added stubs for the rest of the "Classes"

---
+++ lib/model/events/event_class.rb
@@ -0,0 +1,19 @@
+#############################################################################
+# Copyright © 2010 Dan Wanek <dwanek@nd.gov>
+#
+#
+# This file is part of zenoss_client.
+# 
+# zenoss_client is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+# 
+# zenoss_client is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with zenoss_client.  If not, see <http://www.gnu.org/licenses/>.
+#############################################################################
