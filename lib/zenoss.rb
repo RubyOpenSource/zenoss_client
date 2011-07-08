fda39c351fa90e4617ae2d34c59ddccd088700ae
Dan Wanek <dan.wanek@gmail.com>
2011-07-08 14:06:17 -0500

    Added ability to modify HTTPClient options on connect.

--- lib/zenoss.rb
+++ lib/zenoss.rb
@@ -32,8 +32,8 @@ module Zenoss
 
   # initialize a connection to a Zenoss server. This is the same as doing
   #   Zenoss::Connection.new(server,user,pass)
-  def Zenoss.connect(server, user, pass)
-    Connection.new(server,user,pass)
+  def Zenoss.connect(server, user, pass, &block)
+    Connection.new(server,user,pass,&block)
   end
 
   # Some of the REST methods return Strings that are formated like a Python list.
--- lib/zenoss/connection.rb
+++ lib/zenoss/connection.rb
@@ -31,11 +31,12 @@ module Zenoss
     include Zenoss::JSONAPI::ReportRouter
     include Zenoss::RESTAPI
 
-    def initialize(url, user, pass)
+    def initialize(url, user, pass, &block)
       @zenoss_uri = (url.is_a?(URI) ? url : URI.parse(url))
       @request_number = 1
       @httpcli = HTTPClient.new
       @httpcli.receive_timeout = 360  # six minutes should be more that sufficient
+      yield(@httpcli) if block_given?
       sign_in(user,pass)
     end
 
