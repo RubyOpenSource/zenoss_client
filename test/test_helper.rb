5a7fea701652b1066b340fcbb62972e1d3f0b8b1
Ben Miller <bmiller@rackspace.com>
2016-05-11 17:35:09 -0400

    allow using webmock 2 for testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ $:.unshift File.join(File.dirname(__FILE__),'..','lib')
 #http://net.tutsplus.com/tutorials/ruby/ruby-for-newbies-testing-with-rspec/
 require_relative '../lib/zenoss'
 require 'vcr'
-
+require 'webmock'
 
 ZENOSS_URL = ENV['zenoss_client_url'] || "http://localhost:8080/zport/dmd"
 ZENOSS_USER = ENV['zenoss_client_username'] || "admin"
@@ -12,8 +12,8 @@ TEST_DEVICE_NAME = "UnitTestDevice"
 
 ZENOSS_VERSION = ENV['zenoss_version'] || '4.2.5'
 
-
 # VCR
+WebMock.enable!
 VCR.configure do |config|
   config.cassette_library_dir = "test/fixtures/vcr_cassettes"
   config.hook_into :webmock
