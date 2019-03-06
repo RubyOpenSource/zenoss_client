9f84615437dc77b4849c6dada3d962f3a567fb3f
Taylor Etheredge <taylor.etheredge@gmail.com>
2019-03-06 12:49:30 -0600

    remove example of testing Zenoss::Model::Device method

--- test/zenoss_client_test.rb
+++ test/zenoss_client_test.rb
@@ -161,28 +161,6 @@ describe Zenoss do
   end
 end
 
-describe Zenoss do
-  it 'raises error on #set_info when version is less than 6' do
-    opts = {}
-    opts[:version] = '4.2.5'
-    opts[:no_sign_in] = true
-    connection = Zenoss::Connection.new('http://localhost', 'admin', 'zenoss', opts)
-    zhash = {
-      productionState: 400,
-      priority: 3,
-      uid: '/zport/dmd/Devices/Server/devices/UnitTestDevice',
-      name: 'UnitTestDevice'
-    }
-
-    dev = Zenoss::Model::Device.new(connection, zhash)
-    options = {}
-    options[:productionState] = -1
-    exception = assert_raises Zenoss::ZenossError do
-      dev.set_info(options)
-    end
-    assert_equal('setInfo method on DeviceRouter is only allowed for version 6 and above', exception.message)
-  end
-end
 
 describe Zenoss::Events::Event do
   it 'initializes an object with times as strings' do
